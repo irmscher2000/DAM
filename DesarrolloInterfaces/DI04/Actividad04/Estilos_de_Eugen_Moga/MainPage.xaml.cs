9a6a5d6ee006c60e4ce99034b8bdf73e91ae3109
Irmscher2000 <eugen.registros.com>
2026-05-21 00:02:30 +0200

    DI04

---
+++ DesarrolloInterfaces/DI04/Actividad04/Estilos_de_Eugen_Moga/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Estilos_de_Eugen_Moga
+{
+    /// <summary>
+    /// Lógica de interacción para MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Contactar_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = Application.Current.MainWindow as MainWindow;
+            mainWindow?.MainFrame.Navigate(new ContactoPage());
+        }
+    }
+}
