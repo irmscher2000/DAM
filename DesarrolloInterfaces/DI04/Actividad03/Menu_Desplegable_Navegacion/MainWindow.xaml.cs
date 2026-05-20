4d87e2b28f1d8de25a65ab23c6c4ee415f5b10ce
Irmscher2000 <eugen.registros.com>
2026-05-20 23:57:25 +0200

    DI04

---
+++ DesarrolloInterfaces/DI04/Actividad03/Menu_Desplegable_Navegacion/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+
+namespace Menu_Desplegable_Navegacion
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void M_Nuevo_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow NuevaVentana = new MainWindow();
+            NuevaVentana.Show();
+        }
+
+        private void M_Abrir_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog AbrirFichero = new Microsoft.Win32.OpenFileDialog();
+            AbrirFichero.ShowDialog();
+        }
+
+        private void M_Guardar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(" Guardado ");
+        }
+
+        private void M_Guardar_como_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog GuardarFicheroComo = new Microsoft.Win32.SaveFileDialog();
+            GuardarFicheroComo.ShowDialog();
+        }
+
+        private void M_Imprimir_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.PrintDialog Imprimir = new System.Windows.Controls.PrintDialog();
+            Imprimir.ShowDialog();
+        }
+
+        private void M_Salir_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void M_Usuarios_Click(object sender, RoutedEventArgs e)
+        {
+            Usuarios AbrirUsuarios = new Usuarios();
+            AbrirUsuarios.Show();
+        }
+    }
+}
