f101f34c8b9d0ed7d2b7a791a0f58431f4c56fba
Eugen Moga <114355965+irmscher2000@users.noreply.github.com>
2026-05-21 14:35:33 +0200

    Delete DesarrolloWeb/DWEC_03 directory

--- DesarrolloWeb/DWEC_03/Aplicacion1/funciones.js
+++
@@ -1,8 +0,0 @@
-function cargarDatos(){
-    document.getElementById("url").textContent = window.location.href;
-
-    const protocol = window.location.protocol;
-    document.getElementById("protocolo").innerHTML = window.location.protocol;
-    
-    document.getElementById("agente").textContent = navigator.userAgent;
-}
